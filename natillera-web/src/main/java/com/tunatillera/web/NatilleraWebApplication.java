78086f810a758a9c6abd9a31ef59910eb952bc6a
Sair Martinez <sairth19@gmail.com>
2020-03-03 22:36:15 -0500

    Creating Web App

---
+++ natillera-web/src/main/java/com/tunatillera/web/NatilleraWebApplication.java
@@ -0,0 +1,13 @@
+package com.tunatillera.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NatilleraWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NatilleraWebApplication.class, args);
+	}
+
+}
---
+++ natillera-web/src/test/java/com/tunatillera/web/NatilleraWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tunatillera.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NatilleraWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
